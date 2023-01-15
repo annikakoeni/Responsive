7d901b930c3650be08f8565869f57e197739b8f4
annikakoeni <80926929+annikakoeni@users.noreply.github.com>
2023-01-15 23:09:06 +0100

    update

--- script.js
+++ script.js
@@ -12,90 +12,99 @@ const swiper = new Swiper(".swiper", {
 
 swiper.slideTo(1, false, false);
 
-
 //animation
 
+window.addEventListener(
+  "load",
+  (event) => {
+    createObserver();
 
-
-window.addEventListener("load", (event) => {
-
-  createObserver();
-}, false);
+    const generator = document.querySelector("#plant-generator");
+    generator.addEventListener("change", function (event) {
+      console.log("event.target.value", event.target.value);
+      switch (event.target.value) {
+        case "Cinerea":
+          console.log(document.querySelector("#generator-farbe"));
+          break;
+        case "Salix":
+        default:
+          break;
+      }
+    });
+  },
+  false
+);
 
 function createObserver() {
   let observer;
 
- 
-
-
   observer = new IntersectionObserver(handleIntersect);
-  document.querySelectorAll("#boxleft, #boxright, #picleft, #picright, #symbox, #plant4").forEach(element => {
-    observer.observe(element);
-    console.log(element);
-  })
-
+  document
+    .querySelectorAll(
+      "#boxleft, #boxright, #picleft, #picright, #symbox, #plant4, #dreieckanimation"
+    )
+    .forEach((element) => {
+      observer.observe(element);
+    });
 }
-
 
 function handleIntersect(entries, observer) {
   entries.forEach((entry) => {
-    console.log(entry)
-
     if (entry.isIntersecting) {
       switch (entry.target.id) {
-        case 'boxleft':
-          case 'picleft':
-          entry.target.classList.add('left-right-animation');
+        case "boxleft":
+        case "picleft":
+          entry.target.classList.add("left-right-animation");
           break;
-        
-        case 'symbox':
-          entry.target.classList.add('left-right-todo-animation');
+
+        case "symbox":
+        case "dreieckanimation":
+          entry.target.classList.add("left-right-todo-animation");
           break;
-        case 'boxright':
-        case 'picright':
-          entry.target.classList.add('right-left-animation');
+        case "boxright":
+        case "picright":
+          entry.target.classList.add("right-left-animation");
           break;
-        case 'plant4':
-          entry.target.classList.add('right-left-todo-animation');
+        case "plant4":
+          entry.target.classList.add("right-left-todo-animation");
           break;
         default:
           break;
       }
     } else {
       switch (entry.target.id) {
-        case 'boxleft':
-        case 'picleft':
-          entry.target.classList.remove('left-right-animation');
+        case "boxleft":
+        case "picleft":
+          entry.target.classList.remove("left-right-animation");
           break;
-        case 'symbox':
-          entry.target.classList.remove('left-right-todo-animation');
+        case "symbox":
+        case "dreieckanimation":
+          entry.target.classList.remove("left-right-todo-animation");
           break;
-        case 'boxright':
-        case 'picright':
-          entry.target.classList.remove('right-left-animation');
+        case "boxright":
+        case "picright":
+          entry.target.classList.remove("right-left-animation");
           break;
-        case 'plant4':
-          entry.target.classList.remove('right-left-todo-animation');
+        case "plant4":
+          entry.target.classList.remove("right-left-todo-animation");
           break;
         default:
           break;
       }
     }
   });
-
 }
 
-function updateGenerator(type) {
-  switch (type) {
-    case "farbe":
-      document.querySelector(".platzhalter").getElementsByClassName.backgroundImage= "url('pic/k1.png')"
-      
-      break;
-  
-    default:
-      break;
-  }
-
+function updateGenerator() {
+  const art = document.getElementById("generator-art").value;
+  const groesse = document.getElementById("generator-groesse").value;
+  const farbe = document.getElementById("generator-farbe").value;
+  const bildName = `${art}-${groesse}-${farbe}.png`;
+  document.querySelector(".platzhalter").style.backgroundColor = "unset";
+  document.querySelector(
+    ".platzhalter"
+  ).style.backgroundImage = `url(./pic/${bildName})`;
 }
 
-
+// chinera - blau weiß
+// salix - grün lila
